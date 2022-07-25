cdd5276746eee1857ac088c0559c3c50aa924f8d
21mx217 <21mx217@MCA.PSG>
2022-07-25 15:17:02 +0530

    Even number program

---
+++ Main.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main{
+    
+    public static void main(String args[]){
+        int n,i=1;
+        Scanner sc=new Scanner(System.in);
+        n =sc.nextInt();
+        while(i<=n)
+            {
+                if(i%2==0)
+                {
+                  System.out.println(i);
+                }
+                i++;
+            }
+        
+    }
+    
+    
+}
